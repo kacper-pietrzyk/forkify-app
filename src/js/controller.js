9f0359ed2f60e43ab4b2990b26f96c25d25ab241
Kacper Pietrzyk <kacper1609@gmail.com>
2023-06-04 15:47:04 +0200

    get recipe id from url

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,9 +31,11 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    
     // 1) Loading recipe
     renderSpinner(recipeContainer);
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886')
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -150,4 +152,4 @@ const showRecipe = async function () {
   }
 }
 
-showRecipe();
+window.addEventListener('hashchange', showRecipe);
