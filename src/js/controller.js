9ed427271fc9a4df7cfa21114b28f29b9d31e026
Kacper Pietrzyk <kacper1609@gmail.com>
2023-06-04 15:51:01 +0200

    guard clause if there is no id

--- src/js/controller.js
+++ src/js/controller.js
@@ -32,6 +32,8 @@ const renderSpinner = function(parentEl) {
 const showRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
+
+    if (!id) return;
     
     // 1) Loading recipe
     renderSpinner(recipeContainer);
