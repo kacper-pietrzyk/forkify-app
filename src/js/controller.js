254bea8236bc469aa8402e0e4c7cd7d6a169e875
Kacper Pietrzyk <kacper1609@gmail.com>
2023-06-04 19:23:18 +0200

    MVC init

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../img/icons.svg'; // Parcel 2
 import 'core-js/stable'; // pollyfilling everything else
@@ -17,7 +20,7 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const renderSpinner = function(parentEl) {
+const renderSpinner = function (parentEl) {
   const markup = `
   <div class="spinner">
     <svg>
@@ -34,27 +37,11 @@ const showRecipe = async function () {
     const id = window.location.hash.slice(1);
 
     if (!id) return;
-    
-    // 1) Loading recipe
     renderSpinner(recipeContainer);
-    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await res.json();
 
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients
-    };
-
-    console.log(recipe);
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
 
     // 2) Rendering recipe
     const markup = `
