b5194e1c0f6ef1994440c640caa388958febda21
Kacper Pietrzyk <kacper1609@gmail.com>
2023-06-08 19:39:05 +0200

    add resultView to controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import ResultsView from './views/resultsView.js';
 
 import 'core-js/stable'; // pollyfilling everything else
 import 'regenerator-runtime/runtime'; // pollyfilling async/await
+import resultsView from './views/resultsView.js';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +31,7 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function() {
   try {
+    resultsView.renderSpinner();
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -33,12 +40,11 @@ const controlSearchResults = async function() {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
 }
-controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
