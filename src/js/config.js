95f6eab7f4c11284b8b20f686f658d8c41389183
Kacper Pietrzyk <kacper1609@gmail.com>
2023-07-09 13:21:04 +0200

    uploading a new recipe - part 2

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'a444a2c6-4a34-4680-86fb-1a8e731a604c';
+export const MODAL_CLOSE_SEC = 2.5;
